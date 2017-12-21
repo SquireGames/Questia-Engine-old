148cbfbeeab5b472154df3b1967688e1dba1c280
Matthew Kaminski <kaminskimateusz7@gmail.com>
2017-12-21 12:29:01 -0500

    Attempt to fix install issues

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,5 +1,12 @@
 include(downloadProject)
 
+## force visual studio to link with /MT and MTd when linking statically
+# TODO move to dependency repository
+if(LIBRARY_TYPE STREQUAL "static" AND CMAKE_GENERATOR MATCHES "Visual Studio")
+	set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} /MT" PARENT_SCOPE)
+    set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} /MTd" PARENT_SCOPE)
+endif()
+
 # TODO see if the project is already available on the computer
 
 set(PROJ_NAME "qeng-deps")
@@ -13,7 +20,6 @@ downloadProject(
 )
 
 ## TODO: move to dependency repository
-
 set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
 
 # add to include path
@@ -23,32 +29,59 @@ include_directories(
     "${${PROJ_NAME}_SOURCE_DIR}/SDL2-2.0.7/include"
 )
 
+# ensure SDL2 is compiling the correct libraries
+if(BUILD_SHARED_LIBS)
+    set(SDL_SHARED ON)
+else()
+    set(SDL_STATIC ON)
+endif()
 
-# TODO make macro to automate changing output directory
+# give installer for targets the correct binary location
+add_custom_target(copy_lib_files ALL)
+MACRO(COPY_LIBS_TO_BINARY_DIR targetLib)
+    get_property(LIB_BIN_DIR TARGET ${targetLib} PROPERTY BINARY_DIR)
+    get_target_property(LIB_FILE_PREFIX ${targetLib} PREFIX)
+    get_target_property(LIB_FILE_BASENAME ${targetLib} OUTPUT_NAME)
+    get_target_property(LIB_FILE_SUFFIX ${targetLib} SUFFIX)
+    if(LIB_FILE_PREFIX MATCHES "-NOTFOUND")
+        if(BUILD_SHARED_LIBS)
+            set(LIB_FILE_PREFIX ${CMAKE_SHARED_LIBRARY_PREFIX})
+        else()
+            set(LIB_FILE_PREFIX ${CMAKE_STATIC_LIBRARY_PREFIX})
+        endif()
+    endif()
+    if(LIB_FILE_BASENAME MATCHES "-NOTFOUND")
+        set(LIB_FILE_BASENAME ${targetLib})
+    endif()
+    if(LIB_FILE_SUFFIX MATCHES "-NOTFOUND")
+        if(BUILD_SHARED_LIBS)
+            set(LIB_FILE_SUFFIX ${CMAKE_SHARED_LIBRARY_SUFFIX})
+        else()
+            set(LIB_FILE_SUFFIX ${CMAKE_STATIC_LIBRARY_SUFFIX})
+        endif()
+    endif()
+    if(LIB_FILE_SUFFIX MATCHES "-NOTFOUND")
+    endif()
+    set(LIV_NAME_FULL ${LIB_FILE_PREFIX}${LIB_FILE_BASENAME}${LIB_FILE_SUFFIX})
+    add_custom_command(
+        OUTPUT ${LIB_BIN_DIR}/${targetLib}.lib
+        COMMAND ${CMAKE_COMMAND} -E copy
+            $<TARGET_FILE:${targetLib}>
+            ${LIB_BIN_DIR}
+        DEPENDS ${targetLib}
+    )
+    add_custom_target(copy_file_${targetLib} ALL
+        DEPENDS ${LIB_BIN_DIR}/${LIV_NAME_FULL}
+    )
+ENDMACRO()
 
-# ensure install will find all lib files
-set_target_properties(SDL2 PROPERTIES
-    RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin"
-    ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/lib"
-    LIBRARY_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/lib"
-)
-set_target_properties(gtest PROPERTIES
-    RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin"
-    ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/lib"
-    LIBRARY_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/lib"
-)
-set_target_properties(gtest_main PROPERTIES
-    RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin"
-    ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/lib"
-    LIBRARY_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/lib"
-)
-set_target_properties(gmock PROPERTIES
-    RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin"
-    ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/lib"
-    LIBRARY_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/lib"
-)
-set_target_properties(gmock_main PROPERTIES
-    RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin"
-    ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/lib"
-    LIBRARY_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/lib"
-)
+COPY_LIBS_TO_BINARY_DIR(gtest)
+COPY_LIBS_TO_BINARY_DIR(gtest_main)
+COPY_LIBS_TO_BINARY_DIR(gmock)
+COPY_LIBS_TO_BINARY_DIR(gmock_main)
+if(SDL_SHARED)
+    COPY_LIBS_TO_BINARY_DIR(SDL2)
+endif()
+if(SDL_STATIC)
+    COPY_LIBS_TO_BINARY_DIR(SDL2-static)
+endif()
