3e146792f749eac836e3bc6b19497773d793670d
Matthew Kaminski <kaminskimateusz7@gmail.com>
2017-12-21 12:43:44 -0500

    Removed broken lib file copies
    Should fix the CI errors

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -7,6 +7,7 @@ if(LIBRARY_TYPE STREQUAL "static" AND CMAKE_GENERATOR MATCHES "Visual Studio")
     set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} /MTd" PARENT_SCOPE)
 endif()
 
+
 # TODO see if the project is already available on the computer
 
 set(PROJ_NAME "qeng-deps")
@@ -36,52 +37,57 @@ else()
     set(SDL_STATIC ON)
 endif()
 
-# give installer for targets the correct binary location
-add_custom_target(copy_lib_files ALL)
-MACRO(COPY_LIBS_TO_BINARY_DIR targetLib)
-    get_property(LIB_BIN_DIR TARGET ${targetLib} PROPERTY BINARY_DIR)
-    get_target_property(LIB_FILE_PREFIX ${targetLib} PREFIX)
-    get_target_property(LIB_FILE_BASENAME ${targetLib} OUTPUT_NAME)
-    get_target_property(LIB_FILE_SUFFIX ${targetLib} SUFFIX)
-    if(LIB_FILE_PREFIX MATCHES "-NOTFOUND")
-        if(BUILD_SHARED_LIBS)
-            set(LIB_FILE_PREFIX ${CMAKE_SHARED_LIBRARY_PREFIX})
-        else()
-            set(LIB_FILE_PREFIX ${CMAKE_STATIC_LIBRARY_PREFIX})
+# only add new targets to copy files on non-Windows machines
+#if(NOT WIN32)
+    # give installer for targets the correct binary location
+    MACRO(COPY_LIBS_TO_BINARY_DIR targetLib)
+        get_property(LIB_BIN_DIR TARGET ${targetLib} PROPERTY BINARY_DIR)
+        get_target_property(LIB_FILE_PREFIX ${targetLib} PREFIX)
+        get_target_property(LIB_FILE_BASENAME ${targetLib} OUTPUT_NAME)
+        get_target_property(LIB_FILE_SUFFIX ${targetLib} SUFFIX)
+        if(LIB_FILE_PREFIX MATCHES "-NOTFOUND")
+            if(BUILD_SHARED_LIBS)
+                set(LIB_FILE_PREFIX ${CMAKE_SHARED_LIBRARY_PREFIX})
+            else()
+                set(LIB_FILE_PREFIX ${CMAKE_STATIC_LIBRARY_PREFIX})
+            endif()
         endif()
-    endif()
-    if(LIB_FILE_BASENAME MATCHES "-NOTFOUND")
-        set(LIB_FILE_BASENAME ${targetLib})
-    endif()
-    if(LIB_FILE_SUFFIX MATCHES "-NOTFOUND")
-        if(BUILD_SHARED_LIBS)
-            set(LIB_FILE_SUFFIX ${CMAKE_SHARED_LIBRARY_SUFFIX})
-        else()
-            set(LIB_FILE_SUFFIX ${CMAKE_STATIC_LIBRARY_SUFFIX})
+        if(LIB_FILE_BASENAME MATCHES "-NOTFOUND")
+            set(LIB_FILE_BASENAME ${targetLib})
         endif()
-    endif()
-    if(LIB_FILE_SUFFIX MATCHES "-NOTFOUND")
-    endif()
-    set(LIV_NAME_FULL ${LIB_FILE_PREFIX}${LIB_FILE_BASENAME}${LIB_FILE_SUFFIX})
-    add_custom_command(
-        OUTPUT ${LIB_BIN_DIR}/${targetLib}.lib
-        COMMAND ${CMAKE_COMMAND} -E copy
-            $<TARGET_FILE:${targetLib}>
-            ${LIB_BIN_DIR}
-        DEPENDS ${targetLib}
-    )
-    add_custom_target(copy_file_${targetLib} ALL
-        DEPENDS ${LIB_BIN_DIR}/${LIV_NAME_FULL}
-    )
-ENDMACRO()
+        if(LIB_FILE_SUFFIX MATCHES "-NOTFOUND")
+            if(BUILD_SHARED_LIBS)
+                set(LIB_FILE_SUFFIX ${CMAKE_SHARED_LIBRARY_SUFFIX})
+            else()
+                set(LIB_FILE_SUFFIX ${CMAKE_STATIC_LIBRARY_SUFFIX})
+            endif()
+        endif()
+        if(LIB_FILE_SUFFIX MATCHES "-NOTFOUND")
+        endif()
+        set(LIV_NAME_FULL ${LIB_FILE_PREFIX}${LIB_FILE_BASENAME}${LIB_FILE_SUFFIX})
+        add_custom_command(
+            OUTPUT ${LIB_BIN_DIR}/${targetLib}.lib
+            COMMAND ${CMAKE_COMMAND} -E copy
+                $<TARGET_FILE:${targetLib}>
+                ${LIB_BIN_DIR}
+            DEPENDS ${targetLib}
+        )
+        add_custom_target(copy_file_${targetLib} ALL
+            DEPENDS ${LIB_BIN_DIR}/${LIV_NAME_FULL}
+        )
+    ENDMACRO()
 
-COPY_LIBS_TO_BINARY_DIR(gtest)
-COPY_LIBS_TO_BINARY_DIR(gtest_main)
-COPY_LIBS_TO_BINARY_DIR(gmock)
-COPY_LIBS_TO_BINARY_DIR(gmock_main)
-if(SDL_SHARED)
-    COPY_LIBS_TO_BINARY_DIR(SDL2)
-endif()
-if(SDL_STATIC)
-    COPY_LIBS_TO_BINARY_DIR(SDL2-static)
-endif()
+    if(BUILD_GMOCK)
+        COPY_LIBS_TO_BINARY_DIR(gtest)
+    endif()
+    if(BUILD_GTEST)
+        COPY_LIBS_TO_BINARY_DIR(gmock)
+    endif()
+    if(SDL_SHARED)
+        COPY_LIBS_TO_BINARY_DIR(SDL2)
+    endif()
+    if(SDL_STATIC)
+        COPY_LIBS_TO_BINARY_DIR(SDL2-static)
+    endif()
+
+#endif()
