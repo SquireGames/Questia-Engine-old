b6ec48d52bcf5492bc6026662c7fb940fdcefeda
Matthew Kaminski <kaminskimateusz7@gmail.com>
2018-05-09 17:55:02 -0400

    Add dependencies matching shared option of main package

--- conanfile.py
+++ conanfile.py
@@ -5,24 +5,27 @@ class QengConan(ConanFile):
     name = "QENG"
     version = "0.0.1"
     folder = "Questia-Engine"
-    license = "GPL-3.0"
+    license = "https://github.com/SquireGames/Questia-Engine/blob/master/LICENSE.txt"
     url = "https://github.com/SquireGames/Questia-Engine"
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False]}
-    # default_options = "shared=False"
-    default_options = "gtest:shared=True", "shared=True"
+    default_options = "shared=True", "gtest:shared=True"
     generators = "cmake"
     requires = "gtest/1.8.0@bincrafters/stable"
 
+    def configure(self):
+        for req in self.requires:
+            self.options[req.split('/', 1)[0]].shared = self.options.shared
+    
     def source(self):
         self.run("git clone https://github.com/SquireGames/Questia-Engine.git")
-        self.run("cd Questia-Engine")  # TODO && git checkout ~")
+        self.run("cd Questia-Engine")  
 
     def build(self):
-        shared = {"BUILD_SHARED_LIBS": self.options.shared}
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
+        cmake.install()
 
     def package(self):
         self.copy("*.h", dst="include", src=self.folder)
@@ -33,9 +36,10 @@ class QengConan(ConanFile):
         self.copy("*.a", dst="lib", keep_path=False)
 
     def package_info(self):
-        # self.cpp_info.libs = ["hello"]
-        pass
+        self.cpp_info.libs = self.collect_libs()
               
     def imports(self):
-        self.copy("*.dll", "bin", "bin")
-        self.copy("*.dylib", "lib", "lib")
+        if self.options.shared == True:
+            self.copy("*.dll", "bin", "bin")
+            self.copy("*.dylib", "lib", "lib")
+    
