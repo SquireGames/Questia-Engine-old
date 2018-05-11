840b94a8a31461864ed4fb9c3682cb74f625d111
Matthew Kaminski <kaminskimateusz7@gmail.com>
2018-05-10 23:36:28 -0400

    Started adding glfw and glad

--- conanfile.py
+++ conanfile.py
@@ -10,9 +10,9 @@ class QengConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False], "build_doc": [True, False], "build_samples": [True, False],
                "build_tests": [True, False]}
-    default_options = "shared=True", "build_doc=False", "build_samples=False", "build_tests=False", "gtest:shared=True"
+    default_options = "shared=True", "build_doc=False", "build_samples=False", "build_tests=False", "gtest:shared=True", "glfw:shared=True", "glad:shared=True", "glad:profile=core", "glad:api_type=gl", "glad:api_version=3.3", "glad:spec=gl", "glad:no_loader=False"
     generators = "cmake"
-    requires = "gtest/1.8.0@bincrafters/stable"
+    requires = "gtest/1.8.0@bincrafters/stable", "glfw/3.2.1.20180327@bincrafters/stable", "glad/0.1.16a0@bincrafters/stable"
 
     def configure(self):
         for req in self.requires:
@@ -40,9 +40,10 @@ class QengConan(ConanFile):
         self.copy("*.a", dst="lib", keep_path=False)
 
     def package_info(self):
-        self.cpp_info.libs = self.collect_libs()
+        self.cpp_info.libs = tools.collect_libs(self)
 
     def imports(self):
         if self.options.shared:
             self.copy("*.dll", "bin", "bin")
             self.copy("*.dylib", "lib", "lib")
+            
