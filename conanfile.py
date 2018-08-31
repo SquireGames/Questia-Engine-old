464a2ac894c53783d77465c7610d68ca49c00553
Matthew Kaminski <kaminskimateusz7@gmail.com>
2018-08-31 19:07:04 -0400

    Added graphics option to conanfile

--- conanfile.py
+++ conanfile.py
@@ -11,17 +11,16 @@ class QengConan(ConanFile):
         "shared": [True, False],
         "build_doc": [True, False],
         "build_samples": [True, False],
-        "build_tests": [True, False]}
+        "build_tests": [True, False],
+        "graphics": ["none", "opengl"]}
     default_options = (
-        "shared=True", "gtest:shared=True", "glfw:shared=True", "glad:shared=True", 
+        "shared=True",
         "build_doc=False", 
         "build_samples=False",
         "build_tests=False",
-        "glad:profile=core", "glad:api_type=gl", "glad:api_version=3.3", "glad:spec=gl", "glad:no_loader=False")
+        "graphics=opengl")
     generators = "cmake"
-    requires = (
-         "glfw/3.2.1.20180327@bincrafters/stable", 
-         "glad/0.1.24@bincrafters/stable")
+    requires = ()
          
     exports_sources = "*", "!build*", "!test_package*", "!.*", "!conanfile.py"
 
@@ -29,6 +28,18 @@ class QengConan(ConanFile):
         if self.options.build_tests:
             self.requires("gtest/1.8.0@bincrafters/stable")
             self.options["gtest"].shared = self.options.shared
+        if self.options.graphics == "opengl":
+            self.requires("glad/0.1.24@bincrafters/stable")
+            self.requires("glfw/3.2.1.20180327@bincrafters/stable")
+            self.options["glad"].shared = self.options.shared
+            self.options["glfw"].shared = self.options.shared
+            self.options["glad"].profile = "core"
+            self.options["glad"].api_type = "gl"
+            self.options["glad"].api_version = "3.3"
+            self.options["glad"].spec = "gl"
+            self.options["glad"].no_loader = False
+            self.options["glad"].extensions = ""
+
 
     def configure(self):
         for req in self.requires:
