4543079cfa9628911bc3630a3b4d812f57897c69
Matthew Kaminski <kaminskimateusz7@gmail.com>
2019-03-09 15:51:02 -0500

    Updated graphics settings to be modular

--- samples/sample.cpp
+++ samples/sample.cpp
@@ -5,20 +5,17 @@
 #include <vector>
 #include <QENG/graphics/opengl/GLInstance.h>
 #include <QENG/graphics/GraphicsAPI.h>
-#include <QENG/math/Vector2.h>
 #include "QENG/ClientEngine.h"
 #include "QENG/math/FixedVector.h"
 
 int main(int argc, char** argv)
 {
-	{
-		auto a = qe::FixedVector<int, 3>{5, 7, 11};
-		std::cout << std::to_string(a.get<0>()) << std::endl;
-		std::cout << std::to_string(a[1]) << std::endl;
+	auto a = qe::FixedVector<int, 3>{5, 7, 11};
+	std::cout << std::to_string(a.get<0>()) << std::endl;
+	std::cout << std::to_string(a[1]) << std::endl;
 
-		auto b = qe::FixedVector<int, 3>{7, 11, 2};
-		std::cout << std::to_string(a == b) << std::endl;
-	}
+	auto b = qe::FixedVector<int, 3>{7, 11, 2};
+	std::cout << std::to_string(a == b) << std::endl;
 
 	qe::GraphicsAPI api {std::make_unique<qe::GLInstance>()};
 
@@ -27,16 +24,6 @@ int main(int argc, char** argv)
 	{
 		std::cout << monitor.getMonitorName() << std::endl;
 	}
-
-	qe::Vector2i test {1, 3};
-	std::cout << std::to_string(test.x) << std::endl;
-	std::cout << std::to_string(test.y) << std::endl;
-
-	qe::Vector2f a = test.normalize<float>();
-	std::cout << std::to_string(a.x);
-
-	//qe::ClientEngine engine;
-	//engine.run();
 
 	return 0;
 }
