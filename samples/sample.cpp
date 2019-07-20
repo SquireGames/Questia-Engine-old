bdb118c293fac9f977c45df58495659b6e1fa23f
Matthew Kaminski <kaminskimateusz7@gmail.com>
2019-07-20 00:26:53 -0400

    Played around with tickrate pool

--- samples/sample.cpp
+++ samples/sample.cpp
@@ -5,7 +5,7 @@
 #include <vector>
 #include <string>
 #include <GLFW/glfw3.h>
-#include <QENG/graphics/Window.h>
+#include "QENG/graphics/Window.h"
 #include "QENG/graphics/impl/opengl/GraphicsAPIImpl.h"
 #include "QENG/graphics/GraphicsAPI.h"
 #include "QENG/graphics/TextureManager.h"
@@ -16,6 +16,15 @@
 // command queue for rendering, other thread
 
 void render(const float percentToNextUpdate){}
+
+constexpr unsigned int calculateTicksInNextUpdate(const unsigned int previousTicks, const unsigned int ticksPerSecond
+                                      , const unsigned int nthUpdate, const unsigned int updatesPerSecond)
+{
+	// calculate the tick value is closest to the ideal value in the current progression
+	auto idealTicksInPool = (unsigned int) (((float) nthUpdate / (float) updatesPerSecond) * (float) ticksPerSecond);
+	// find how many ticks are required to reach the ideal amount
+	return idealTicksInPool - previousTicks;
+}
 
 int main(int argc, char** argv)
 {
@@ -28,7 +37,7 @@ int main(int argc, char** argv)
 
 	auto textureManager = qe::TextureManager();
 
-	const int updatesPerSecond = 60;
+	const unsigned int updatesPerSecond = 60;
 
 	auto currentTime = std::chrono::duration_cast<std::chrono::duration<long, std::micro>>(std::chrono::high_resolution_clock::now().time_since_epoch());
 	auto timePerFrame = std::chrono::duration<long, std::micro>(1'000'000/updatesPerSecond);
@@ -36,9 +45,10 @@ int main(int argc, char** argv)
 	auto timeAccumulator = std::chrono::duration<long, std::micro>(0);
 	auto totalTimePassed = std::chrono::duration<long, std::micro>(0);
 
-	// every second, this will be emptied into the update function.
-	// this allows every tick rate (e.g. 60fps, 100fps) to have the same amount of 'ticks' per second
-	unsigned int tickPool = 1000;
+	// a tick pool allows every frame rate/update rate (e.g. 60fps, 100fps) to have the same amount of 'ticks' per second
+	const unsigned int ticksPerSecond = 1000;
+	unsigned int tickPoolProgress = 0;
+	unsigned int nthUpdate = 0;
 
 	while(!window.shouldClose())
 	{
@@ -59,17 +69,28 @@ int main(int argc, char** argv)
 			unsigned int ticks;
 
 			// every time a second passes, NOTE this will only work when the tick rate is greater than or equal to once per second
-			// this is very useful for ensuring deterministic timesteps
+			// this is  useful for ensuring deterministic timesteps
 			if(std::chrono::duration_cast<std::chrono::seconds>(oldTime) != std::chrono::duration_cast<std::chrono::seconds>(totalTimePassed))
 			{
-				// empty out the rest of the tickPool into ticks
-				ticks = tickPool;
-				tickPool = 1000;
+				// finish whats left of the tickBucket
+				ticks = calculateTicksInNextUpdate(tickPoolProgress, ticksPerSecond, nthUpdate + 1, updatesPerSecond);
+
+				// TODO remove begin
+				tickPoolProgress += ticks;
+				std::cout << std::to_string(tickPoolProgress) << std::endl;
+				// TODO remove end
+
+				//empty out the tick bucket
+				tickPoolProgress = 0;
+				nthUpdate = 0;
+
 				std::cout << std::to_string(totalTimePassed.count()) << std::endl;
 			}
 			else
 			{
-				ticks = updatesPerSecond / 1000;
+				ticks = calculateTicksInNextUpdate(tickPoolProgress, ticksPerSecond, (nthUpdate += 1), updatesPerSecond);
+				tickPoolProgress += ticks;
+				std::cout << std::to_string(tickPoolProgress) << std::endl;
 			}
 
 			api.pollEvents();
